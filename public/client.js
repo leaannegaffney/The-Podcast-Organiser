66302241f83c436949e6b9d052da498df48164d6
Glitch (lgaffney-podcast-playlist) <none>
2019-03-05 13:20:01 +0000

    🎳🏄 Checkpoint ./public/client.js:2968912/380 ./views/layouts/main.hbs:2968912/1

--- public/client.js
+++ public/client.js
@@ -1,7 +1,8 @@
+$(".delepisode").click(function(){
+  return confirm('Really delete this episode?');
+});
+
 $(".delplaylist").click(function(){
   return confirm('Really delete this playlist?');
 });
 
-$(".delepisode").click(function(){
-  return confirm('Really delete this episode?');
-});
