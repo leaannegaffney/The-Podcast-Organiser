7000d8ec4492f87f1d806a97c04fdc2e619235b5
Glitch (lgaffney-podcast-playlist) <none>
2019-03-26 12:15:50 +0000

    🦃🌼 Checkpoint ./controllers/accounts.js:2968912/4 ./routes.js:2968912/402

--- routes.js
+++ routes.js
@@ -7,16 +7,20 @@ const start = require('./controllers/start.js');
 const dashboard = require('./controllers/dashboard.js');
 const about = require('./controllers/about.js');
 const playlist = require('./controllers/playlist.js');
+const accounts = require ('./controllers/accounts.js');
 
-router.get('/', start.index);
+router.get('/start', start.index);
 router.get('/dashboard', dashboard.index);
 router.get('/about', about.index);
+
 router.get('/playlist/:id', playlist.index);
 router.get('/playlist/:id/deleteEpisode/:episodeid', playlist.deleteEpisode);
-router.get('/deletePlayList/:id', playlist.deletePlayList);
 router.post('/playlist/:id/addepisode', playlist.addEpisode);
-router.post('/playlist/addplaylist', playlist.addPlaylist);
 router.post('/playlist/:id/updateepisode/:episodeid', playlist.updateEpisode);
+
+router.get('/deletePlayList/:id', playlist.deletePlayList);
+router.post('/playlist/addplaylist', playlist.addPlaylist);
+
 
 router.get('/', accounts.index);
 router.get('/login', accounts.login);
