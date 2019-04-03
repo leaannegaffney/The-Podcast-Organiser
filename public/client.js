c0d14f418b89ee33afe8b16ac00c84bbf137f880
Glitch (podcast-with-images2) <none>
2019-04-03 16:47:50 +0000

    🕰🏨 Checkpoint ./public/client.js:2968912/156 ./views/start.hbs:2968912/590 ./views/index.hbs:2968912/499

--- public/client.js
+++ public/client.js
@@ -5,11 +5,3 @@ $(".delepisode").click(function(){
 $(".delplaylist").click(function(){
   return confirm('Really delete this playlist?');
 });
-
-function validateForm() {
-  var x = document.forms["myForm"]["fname"].value;
-  if (x == "") {
-    alert("Name must be filled out");
-    return false;
-  }
-}
