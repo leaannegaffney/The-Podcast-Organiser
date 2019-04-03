c3463b3f4b25c7c9350ec0c042c5d20787547fdd
Glitch (podcast-with-images2) <none>
2019-04-03 17:31:49 +0000

    🌏⛺️ Checkpoint ./views/login.hbs:2968912/19 ./views/partials/addplaylist.hbs:2968912/23 ./views/partials/addepisode.hbs:2968912/10 ./controllers/accounts.js:2968912/115

--- controllers/accounts.js
+++ controllers/accounts.js
@@ -61,8 +61,7 @@ const accounts = {
 
   authenticate(request, response) {
   const user = userstore.getUserByEmail(request.body.email);
-  const password = userstore.getUserByPassword(request.body.password);
-    if (user){
+    if ((user)&& user.password === request.body.password){
       response.cookie('playlist', user.email);
       logger.info(`logging in ${user.email}`);
       response.redirect('/start');
